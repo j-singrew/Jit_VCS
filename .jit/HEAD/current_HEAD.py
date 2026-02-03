b1a63eadc648654166c550b5f5af708a7b9a08e4
topshager <rem28777@gmail.com>
2026-02-03 16:44:47 +0200

    Create Curerent head reader ,writer

---
+++ .jit/HEAD/current_HEAD.py
@@ -0,0 +1,33 @@
+from pathlib2 import Path
+
+FILE = Path(".HEAD")
+
+def read_head() -> bytes | None:
+    if not FILE.exists():
+        return None
+    return bytes.fromhex(FILE.read_text().strip())
+
+
+def write_head(oid: bytes) -> None:
+    """Update HEAD to point to a new commit"""
+    FILE.write_text(oid.hex())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
