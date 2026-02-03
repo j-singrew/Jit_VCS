55a341b3b9ce9d4d9c4e5e09070a5e2f0513aad4
topshager <rem28777@gmail.com>
2026-02-03 09:42:46 +0200

    Update main.py

--- .jit/main.py
+++ .jit/main.py
@@ -1,2 +1,9 @@
 from utils import hashing,serialization
 
+def main_commit(command):
+
+    #Pass commit 
+
+    serialised_data = serialization.serialization(command)
+    commit          = hashing.Hash_OID(serialised_data)
+    
