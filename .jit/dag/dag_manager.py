d5153eb4922ffa454c2c9540543fafd2e0995ec7
topshager <rem28777@gmail.com>
2026-02-03 16:45:26 +0200

    Update dag manager - add  current head handler

--- .jit/dag/dag_manager.py
+++ .jit/dag/dag_manager.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass 
 from typing import List    
+from dag import dag_manager
 
 #kay are node 
 #commit_object = {"oid":oid,"parens":CommitData.parents,"state_hash":CommitData.state_hash,"timestamp":CommitData.timestamp}
@@ -12,6 +13,13 @@ class Node:
     oid: bytes
     depends_on:List[str]
 
+
+@dataclass
+class HEAD:
+    def __init__(self,oid):
+        self.Head = oid
+
+
 DAG = {} 
 
 def DAG(commit_object):
@@ -19,6 +27,10 @@ def DAG(commit_object):
     dag_commit = Node(commit_object["oid"],commit_object["parents"])
 
     DAG[dag_commit.oid] = dag_commit
+    Head =  HEAD(dag_commit.oid)
+    dag_manager.UpdateHead(Head)
+
+
 
     return DAG
 
