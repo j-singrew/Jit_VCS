de20282a110152a51fc74b244dd77c8c038adde9
topshager <rem28777@gmail.com>
2026-02-03 07:58:16 +0200

    Move Create hashing.py

---
+++ .jit/utils/hashing.py
@@ -0,0 +1,16 @@
+import hashlib
+
+def Hash_OID(serialised_data:bytes):
+    content = serialised_data
+    hash_object  = hashlib.sha256(content)
+    hex_dig = hash_object.hexdigest()
+    return hex_dig
+
+
+
+    
+
+
+
+
+
