5f8789533264227757ecebd7a7bd39b0f0f84cd6
topshager <rem28777@gmail.com>
2026-02-03 09:28:53 +0200

    Update serialization

--- .jit/utils/serialization.py
+++ .jit/utils/serialization.py
@@ -2,5 +2,23 @@
 
 def serialization(CommitData):
 
-    
+    lines =[]
+
+    lines.append("commit-v1")
+
+    if not CommitData.parents:
+        lines.append("parent NONE")
+    else:
+        for parent_oid in CommitData.parents:
+            lines.append(f"parent {parent_oid}")
+
+    lines.append(f"state {CommitData.state_hash}")
+    lines.append(f"time {CommitData.timestamp}")
+
+    serialized = "\n".join(lines).encode("utf-8")
+
+    return serialized
+
+
+
 
