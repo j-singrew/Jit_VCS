22ccb53a5234ea2874bed7cc1704d7c346f66ace
topshager <rem28777@gmail.com>
2026-02-08 12:38:09 +0200

    coNFIG  CHANGES

--- .jit/main.py
+++ .jit/main.py
@@ -1,6 +1,17 @@
 from utils import hashing,serialization
 from dag import dag_manager
 from HEAD import current_HEAD
+import time
+from objects import commit
+
+# For a first commit, no parents
+p = commit.Commit(
+    parents=[],                # root commit
+    state_hash=b"testhash",    # placeholder state
+    timestamp=int(time.time()), # current unix time
+    message="Initial commit"    # optional
+)
+
 
 def main_commit(CommitData):
 
@@ -9,10 +20,15 @@ def main_commit(CommitData):
 
     serialised_data = serialization.serialization(CommitData)
     oid        = hashing.Hash_OID(serialised_data)
+    print("oid:",oid)
 
     commit_object = {"oid":oid,"parents":Current_Head,"state_hash":CommitData.state_hash,"timestamp":CommitData.timestamp}
 
     DAG_creation =  dag_manager.Dag(commit_object)
 
+
+
+def __main__(p): 
+    main_commit(p)
 
 
--- .jit/storage/find_manager.py
+++ .jit/storage/find_manager.py
@@ -2,13 +2,14 @@ from persistant_storage import *
 from storage_manager import  exists, paths_for_oid
 from utils import serialization
 
-def location_orchestration(oid: bool):
+def location_orchestration(oid: bytes):
 
     if exists(oid):
        _, file_path =  paths_for_oid(oid)
-       raw_file = open("{file_path}","rb")
-       raw_content = raw_file.read_bytes()
-       serialization.deserialization(raw_content)
+       raw_file = open(file_path,"rb")
+       raw_content = raw_file.read()
+       commit_obj =  serialization.deserialization(raw_content)
+       return commit_obj
 
 
     else:
