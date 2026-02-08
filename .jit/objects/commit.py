7195e7766459a23b3f3d9ab99181aad7feefef22
topshager <rem28777@gmail.com>
2026-02-08 11:46:18 +0200

    Update commit.py

--- .jit/objects/commit.py
+++ .jit/objects/commit.py
@@ -1,10 +1,10 @@
 #Each commit represents a snapshot of the project at a specific time.
 #Git records metadata like author, timestamp, commit message, and parent commit.
-
+from dataclasses import dataclass
+from typing import List, Optional
+@dataclass(frozen=True)
 class Commit:
-    parents = None
-    state_hash= None
-    message = None
-    timestamp = None
-    authorship_data = None
-
+    parents: List[bytes]        
+    state_hash: bytes             
+    timestamp: int                
+    message: Optional[str] = None
