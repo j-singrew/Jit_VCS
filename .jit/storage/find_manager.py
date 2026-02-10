f991deb514ed6569a6a495d9344f5923eb3cc126
topshager <rem28777@gmail.com>
2026-02-10 23:45:08 +0200

    Update variable

--- .jit/storage/find_manager.py
+++ .jit/storage/find_manager.py
@@ -1,9 +1,10 @@
 from storage import  storage_manager
 from utils import serialization
 
-def location_orchestration(oid: bytes):
+def location_orchestration(oid:str):
 
-    if storage_manager.exists(oid):
+    if storage_manager.exist(oid):
+       
        _, file_path =  storage_manager.paths_for_oid(oid)
        raw_file = open(file_path,"rb")
        raw_content = raw_file.read()
