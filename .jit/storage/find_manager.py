9f001e5edfaa9decf04f9fa76c1a495407616151
topshager <rem28777@gmail.com>
2026-02-11 10:13:32 +0200

    Update fin_manager.py Accomodate verification

--- .jit/storage/find_manager.py
+++ .jit/storage/find_manager.py
@@ -1,14 +1,20 @@
-from storage import  storage_manager,oid_verify
+from storage import  storage_manager,oid_verification
 from utils import serialization
+from utils import hashing
 from pathlib import Path
 
-def location_orchestration(serialised_data:bytes):
-    _, file_path =  storage_manager.paths_for_oid(serialised_data)
+def location_orchestration(byte_oid:bytes,oid:str):
+    _, file_path =  storage_manager.paths_for_oid(byte_oid)
     if file_path.exists():
        raw_file = open(file_path,"rb")
        raw_content = raw_file.read()
-       commit_obj =  serialization.deserialization(raw_content)
-       return commit_obj
+
+       if  oid_verification.oid_verification(raw_content,oid):
+            commit_obj =  serialization.deserialization(raw_content)
+            return commit_obj
+       else:
+            raw_file.close()
+            raise Exception(f"file with OID {oid} failed verification")
 
 
     else:
