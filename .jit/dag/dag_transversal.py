de41f9253c8d18000c75c8570943102480cd2735
topshager <rem28777@gmail.com>
2026-03-06 17:04:09 +0200

    Update dag_transversal.py

--- .jit/dag/dag_transversal.py
+++ .jit/dag/dag_transversal.py
@@ -3,9 +3,9 @@ from  storage import find_manager
 #DFS - later implementatio
 
 
-def transverse(curr_Head):
-    str_curr = curr_Head.decode('utf-8')
-    file1 = find_manager.location_orchestration(curr_Head ,str_curr)
+def transverse(curr_Head:bytes):
+    
+    file1 = find_manager.location_orchestration(curr_Head ,curr_Head.decode('utf-8'))
     print(file1)
     parent = file1.parents
 
