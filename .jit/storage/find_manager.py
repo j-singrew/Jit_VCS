13c3746ada23bc76935ddb80ca785a1e1fe88b9b
topshager <rem28777@gmail.com>
2026-03-06 08:17:38 +0200

    Commit

--- .jit/storage/find_manager.py
+++ .jit/storage/find_manager.py
@@ -2,6 +2,21 @@ from storage import  storage_manager,oid_verification
 from utils import serialization
 from utils import hashing
 from pathlib import Path
+def DataExtraction(commit_obj):
+    path = []
+    current = commit_obj.stateHash
+    byte_str = str(current)
+    
+    while True:
+        commit_obj = location_orchestration(current,byte_str)
+        path.append((current, commit_obj))
+        if not commit_obj.parents:   
+            print("We found root")
+            break
+        current = commit_obj.parents[0]
+
+    return path
+ 
 
 def location_orchestration(byte_oid:bytes,oid:str):
     _, file_path =  storage_manager.paths_for_oid(byte_oid)
@@ -11,7 +26,6 @@ def location_orchestration(byte_oid:bytes,oid:str):
 
        if  oid_verification.oid_verification(raw_content,oid):
             commit_obj =  serialization.deserialization(raw_content)
-            DataExtraction(commit_obj)
             return commit_obj
        else:
             raw_file.close()
@@ -21,10 +35,5 @@ def location_orchestration(byte_oid:bytes,oid:str):
     else:
         raise Exception(f"file with OID {file_path} does not exist")
     
-def DataExtraction(commit_obj):
-    
-    content_hash  = commit_obj.stateHash
-    timestamp = commit_obj.timeStamp
-    print("hash: ",content_hash)
-    print("timestamps :",timestamp)
+
     
